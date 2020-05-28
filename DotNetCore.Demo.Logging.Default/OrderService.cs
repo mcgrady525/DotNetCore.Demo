27565c107fe1d6eb91f004d996b2c87b3e3af21a
鲁宁 <mcgrady_525@hotmail.com>
2020-05-28 13:53:46 +0800

    使用默认的日志框架记录日志，演示了两种方式获取日志记录器对象。

---
+++ DotNetCore.Demo.Logging.Default/OrderService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNetCore.Demo.Logging.Default
+{
+    public class OrderService
+    {
+        private readonly ILogger<OrderService> _logger;
+
+        public OrderService(ILogger<OrderService> logger)
+        {
+            _logger = logger;
+        }
+
+        public void WriteLog()
+        {
+            _logger.LogDebug("log from orderService!");
+        }
+    }
+}
---
+++ DotNetCore.Demo.Logging.Default/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Configuration;
+using System;
+
+namespace DotNetCore.Demo.Logging.Default
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //读取配置
+                IConfigurationBuilder configBuilder = new ConfigurationBuilder();
+                configBuilder.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                var configRoot = configBuilder.Build();
+
+                //将配置对象注册到容器
+                IServiceCollection serviceCollection = new ServiceCollection();
+                //serviceCollection.AddSingleton<IConfiguration>(p => configRoot);//???
+
+                //注册日志服务
+                serviceCollection.AddLogging(builder =>
+                {
+                    builder.AddConfiguration(configRoot.GetSection("Logging"));
+                    builder.AddConsole();
+                });
+                serviceCollection.AddTransient<OrderService>();
+
+                ////从容器获取日志工厂对象
+                IServiceProvider provider = serviceCollection.BuildServiceProvider();
+                //ILoggerFactory loggerFactory = provider.GetService<ILoggerFactory>();
+
+                ////记录日志，通过CreateLogger获取日志记录器对象
+                //var logger = loggerFactory.CreateLogger("alogger");
+                //logger.LogDebug("this is a debug log!");
+
+                //记录日志，通过构造函数注入获取日志记录器对象
+                var orderService = provider.GetService<OrderService>();
+                orderService.WriteLog();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            //Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
