f0e8919f14c4aaf17c35402243e79a8f42f3e68b
鲁宁 <mcgrady_525@hotmail.com>
2020-05-28 15:29:59 +0800

    增加日志作用域sample。

--- DotNetCore.Demo.Logging.Default/Program.cs
+++ DotNetCore.Demo.Logging.Default/Program.cs
@@ -15,6 +15,7 @@ namespace DotNetCore.Demo.Logging.Default
                 //读取配置
                 IConfigurationBuilder configBuilder = new ConfigurationBuilder();
                 configBuilder.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+                configBuilder.AddCommandLine(args);
                 var configRoot = configBuilder.Build();
 
                 //将配置对象注册到容器
@@ -26,6 +27,7 @@ namespace DotNetCore.Demo.Logging.Default
                 {
                     builder.AddConfiguration(configRoot.GetSection("Logging"));
                     builder.AddConsole();
+                    builder.AddDebug();
                 });
                 serviceCollection.AddTransient<OrderService>();
 
@@ -38,8 +40,18 @@ namespace DotNetCore.Demo.Logging.Default
                 //logger.LogDebug("this is a debug log!");
 
                 //记录日志，通过构造函数注入获取日志记录器对象
-                var orderService = provider.GetService<OrderService>();
-                orderService.WriteLog();
+                //var orderService = provider.GetService<OrderService>();
+                //orderService.WriteLog();
+
+                //日志作用域
+                var logger = provider.GetService<ILogger<Program>>();
+                using (logger.BeginScope(string.Format("ScopeId:{0}", Guid.NewGuid())))
+                {
+                    logger.LogInformation("this is a information log!");
+                    logger.LogError("this is a error log!");
+                    logger.LogCritical("this is a critical log!");
+                }
+
 
             }
             catch (Exception ex)
