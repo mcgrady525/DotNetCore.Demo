7a4d95f2135a799b65a5a2b06faf0bb9aa7c84cd
鲁宁 <mcgrady_525@hotmail.com>
2020-08-05 15:55:40 +0800

    添加身份认证demo。

---
+++ DotNetCore.Demo.Authentication/Controllers/AccountController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCore.Demo.Authentication.Controllers
+{
+    public class AccountController : Controller
+    {
+        //虚拟用户信息
+        private static List<User> _users = new List<User>()
+        {
+            new User {  Id=1, Name="alice", Password="alice", Email="alice@gmail.com", PhoneNumber="18800000001" },
+            new User {  Id=2, Name="bob", Password="bob", Email="bob@gmail.com", PhoneNumber="18800000002" }
+        };
+
+        public AccountController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Login()
+        {
+            var isAuthenticated = User.Identity.IsAuthenticated;
+            if (isAuthenticated)
+            {
+                return Redirect("/Home/Index");
+            }
+
+
+            return View();
+        }
+
+        /// <summary>
+        /// 登录
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public async Task<IActionResult> Login(string userName, string password)
+        {
+            //校验用户名和密码
+            var user = _users.FirstOrDefault(p => p.Name == userName && p.Password == password);
+            if (user == null)
+            {
+                return Content("用户名或密码错误!");
+            }
+
+            //将身份信息写入cookie
+            var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+            identity.AddClaim(new Claim(ClaimTypes.Sid, user.Id.ToString()));
+            identity.AddClaim(new Claim(ClaimTypes.Name, user.Name));
+            var userPrincipal = new ClaimsPrincipal(identity);
+            await HttpContext.SignInAsync(
+                CookieAuthenticationDefaults.AuthenticationScheme,
+                userPrincipal,
+                new AuthenticationProperties
+                {
+                    IsPersistent = true,
+                    AllowRefresh = true,
+                    ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(20)
+                });
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public async Task<IActionResult> LogOut()
+        {
+            await HttpContext.SignOutAsync();
+            return Redirect("/Account/Login");
+        }
+    }
+
+    /// <summary>
+    /// 用户
+    /// </summary>
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Password { get; set; }
+
+        public DateTime Birthday { get; set; }
+    }
+}
---
+++ DotNetCore.Demo.Authentication/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DotNetCore.Demo.Authentication.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace DotNetCore.Demo.Authentication.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 个人信息
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Profile()
+        {
+            var profile = new Profile() {Claims= new Dictionary<string, string>() };
+            profile.Name = HttpContext.User.Identity.Name;
+            profile.AuthenticationType = HttpContext.User.Identity.AuthenticationType;
+
+            var claims= HttpContext.User.Claims.ToList();
+            foreach (var item in claims)
+            {
+                profile.Claims.Add(item.Type, item.Value);
+            }
+
+            return View(profile);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+
+    public class Profile
+    { 
+        public string Name { get; set; }
+
+        public string AuthenticationType { get; set; }
+
+        public Dictionary<string, string> Claims { get; set; }
+    }
+}
